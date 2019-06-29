252d42c33e30a465d2ccba40fcf173c8f4331957
gfw-breaker <lixiaoming0513@gmail.com>
2019-06-29 09:36:42 +0800

    ok

--- release.sh
+++ release.sh
@@ -12,8 +12,8 @@ cat > $index << EOF
 ---
 ##### 友情链接：[免翻墙看禁闻](https://github.com/gfw-breaker/banned-news) &nbsp;|&nbsp; [手把手翻墙教程](https://github.com/gfw-breaker/guides/wiki) &nbsp;|&nbsp;[《江峰时刻》会员网站](https://www.jfengtime.com/) &nbsp;|&nbsp; [白玫瑰传单](https://github.com/gfw-breaker/DieWeisseRose) &nbsp;|&nbsp; [明慧期刊](https://github.com/gfw-breaker/mh-qikan)
 
-| 视频节目名称 | 视频/音频 | 简体字幕 | 正體字幕 |
-|---|---|---|---|
+| 视频节目名称 | 视频/音频 | 简体字幕 | 正體字幕 | 简体文稿 |
+|---|---|---|---|---|
 EOF
 
 ## generate
@@ -32,8 +32,13 @@ for lang in $langs; do
 			txtTw="$dir/$vid.tw.txt"
 			cat $srt | grep -v ^$ | egrep -v ' --> [0-9]+:' | egrep -v ^[0-9]+$  > $txt
 			opencc -c s2tw.json -i $srt -o $srtTw
-			opencc -c s2tw.json -i $txt -o $txtTw
-			echo "| $desc | [下载](https://y2mate.com/zh-cn/search/$id) | [下载]($srt?raw=true) | [下載]($srtTw?raw=true) |" >> $index
+			#opencc -c s2tw.json -i $txt -o $txtTw
+			if [[ ! $vid =~ ^e ]]; then
+				txtLink=" [下载]($txt?raw=true) "
+			else 
+				txtLink="-"
+			fi
+			echo "| $desc | [下载](https://y2mate.com/zh-cn/search/$id) | [下载]($srt?raw=true) | [下載]($srtTw?raw=true) | $txtLink |" >> $index
 		done < $readme
 	done
 done
